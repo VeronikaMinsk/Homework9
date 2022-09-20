bb9d19444bf0efd2e84026a79fbcebd7c952772a
Veronika <griva.verona@gmail.com>
2022-09-20 18:39:43 +0300

    add Task68

---
+++ Task3(68)/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+int m = InputNumber("Введите m: ");
+int n = InputNumber("Введите n: ");
+Console.WriteLine(AckerFunc(n, m));
+
+int InputNumber(string a)
+{
+	Console.WriteLine(a);
+	return Convert.ToInt32(Console.ReadLine());
+}
+
+int AckerFunc(int n, int m)
+{
+	if (n == 0) 
+        return m + 1;
+	else if ((n != 0) && (m == 0)) 
+        return AckerFunc(n - 1, 1);
+	else 
+        return AckerFunc(n - 1, AckerFunc(n, m - 1));
+}
+
