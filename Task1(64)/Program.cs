f8cd6811508bd0208a018d17f7a1ad747e5c06a6
Veronika <griva.verona@gmail.com>
2022-09-20 17:58:59 +0300

    add Task64

---
+++ Task1(64)/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+
+Console.WriteLine("Enter M: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter N: ");
+int last = Convert.ToInt32(Console.ReadLine());
+
+numbersRow(first, last);
+
+int numbersRow(int m, int n)
+{
+	if (m > n) 
+        return n;
+	else
+	{
+		Console.Write(m + " ");
+		m++;
+	}
+	return numbersRow(m, n);
+}
+
+
+
+
